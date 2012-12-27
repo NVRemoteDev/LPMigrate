be3f774a90fae6c83102764ab5d87cb78d422ce9
Anthony Hill <pimsh0@Hotmail.com>
2012-12-26 23:25:00 -0800

    Initial commit

---
+++ LPSD Product Migration/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace LPSD_Migration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.saveCSVButton = new System.Windows.Forms.Button();
+            this.outputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(254, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(184, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start Pulling Data";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(12, 292);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(39, 13);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "Output";
+            // 
+            // saveCSVButton
+            // 
+            this.saveCSVButton.Location = new System.Drawing.Point(254, 295);
+            this.saveCSVButton.Name = "saveCSVButton";
+            this.saveCSVButton.Size = new System.Drawing.Size(184, 23);
+            this.saveCSVButton.TabIndex = 3;
+            this.saveCSVButton.Text = "Save CSV File";
+            this.saveCSVButton.UseVisualStyleBackColor = true;
+            // 
+            // outputRichTextBox
+            // 
+            this.outputRichTextBox.Location = new System.Drawing.Point(12, 41);
+            this.outputRichTextBox.Name = "outputRichTextBox";
+            this.outputRichTextBox.Size = new System.Drawing.Size(668, 248);
+            this.outputRichTextBox.TabIndex = 4;
+            this.outputRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 383);
+            this.Controls.Add(this.outputRichTextBox);
+            this.Controls.Add(this.saveCSVButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button saveCSVButton;
+        private System.Windows.Forms.RichTextBox outputRichTextBox;
+    }
+}
+
---
+++ LPSD Product Migration/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+
+/* Saving for delegation method
+this.Invoke((MethodInvoker)delegate
+{
+    System.Diagnostics.Debug.Write("This program is expected to throw WebException on successful run." +
+        "\n\nException Message :" + e.Message);
+});
+                     
+Saving for streamwriter method
+using (StreamWriter writer = new StreamWriter(@"C:\Users\Desktop\Desktop\LPProductURLs.txt"))
+{
+    writer.WriteLine(fuskUrl);
+}
+*/
+
+namespace LPSD_Migration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Declare our worker thread
+        private Thread workerThread = null;
+
+        // Set the website using the AgilityPackWebsite class then turn it over to the fusk method
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            // Initialise and start worker thread
+            this.workerThread = new Thread(new ThreadStart(this.FuskSetup));
+            this.workerThread.Start();
+        }
+
+        // Set the numerical fusk criteria
+        public void FuskSetup()
+        {
+            FuskMethodCriteriaProperties fusk = new FuskMethodCriteriaProperties();
+            AgilityPackWebsite agility = new AgilityPackWebsite();
+            agility.Website = "http://www.lockhartphillipsusa.com/store/product.php?productid=";
+
+            // Ints only
+            fusk.Start = 17028;
+            fusk.End = 17029;
+
+            // Prep the fusk list with url prepended from agility
+            List<string> fuskList = new List<string>();
+            for (int i = fusk.Start; i <= fusk.End; i++)
+            {
+                fuskList.Add(agility.Website + i.ToString());
+            }
+
+            StartFusking(fuskList);
+        }
+
+        // Start fusking based on the fusk criteria
+        public void StartFusking(List<string> fuskList)
+        {
+            WebClient client = new WebClient();
+
+            string htmlCode;
+
+            // Pull HTML Code
+            foreach (string fuskUrl in fuskList)
+            {
+                try
+                {
+                    htmlCode = client.DownloadString(fuskUrl);
+                }
+                catch (WebException)
+                {
+                    continue;
+                }
+                // URL is good, let's parse it with agilitypack and get the data we want.
+                StartParsingWithAgilityPack(htmlCode);
+            }
+        }
+
+        // Parse
+        public void StartParsingWithAgilityPack(string html)
+        {
+            // Load up HTML
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+
+            // Parse for the SKU
+            string productSku = "";
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//*[@id=\"product_code\"]"))
+            {
+                productSku = link.InnerText;
+                //System.Diagnostics.Debug.Write(link.InnerText + "\n");
+            }
+            // Parse for the main image
+            string mainImageUrl = "";
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//*[@id=\"product_thumbnail\"]"))
+            {
+                HtmlAttribute att = link.Attributes["src"];
+                mainImageUrl = att.Value;
+                //System.Diagnostics.Debug.Write(mainImageUrl + "\n");
+            }
+            // Parse for the detailed images
+            List<string> detailedImages = new List<string>();
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//*[@class=\"detailed-images-other\"]"))
+            {
+                HtmlAttribute att = link.Attributes["src"];
+                detailedImages.Add(att.Value); // This is parsing & signs with &amp; must be aware of this.
+                //System.Diagnostics.Debug.Write(att.Value + "\n");
+            }
+            // Everything is parsed send it to post processing
+            ProcessTheProductDataAndDownloadImages(productSku, mainImageUrl, detailedImages);
+        }
+
+        // Process
+        public void ProcessTheProductDataAndDownloadImages(string sku, string mainImageUrl, List<string> detailedImages)
+        {
+            // Create image dir
+            try
+            {
+                System.IO.Directory.CreateDirectory(@"C:\LPUSA Migration\" + sku);
+            }
+            catch (System.IO.IOException e)
+            {
+                System.Diagnostics.Debug.Write(e.Message);
+            }
+
+            // Download main image
+            mainImageUrl = mainImageUrl.Replace("&amp;", "&");
+            WebClient client = new WebClient();
+            try
+            {
+                client.DownloadFile(mainImageUrl, @"C:\LPUSA Migration\" + sku + @"\main.jpg");
+            }
+            catch (WebException e)
+            {
+                System.Diagnostics.Debug.Write(e.Message);
+            }
+
+            // Download detailed images and write them
+            string nonLocalDetailedImageUrl = "";
+            for (int i = 0; i < detailedImages.Count; i++)
+            {
+                // &amp; fix
+                detailedImages[i] = detailedImages[i].Replace("&amp;", "&");
+
+                nonLocalDetailedImageUrl = detailedImages[i];
+                // Remove broken image links
+                if (detailedImages[i] == "/store/default_image.gif")
+                {
+                    continue;
+                }
+                // Download detailed image and name file to SKU 
+                try
+                {
+                    client.DownloadFile(detailedImages[i], @"C:\LPUSA Migration\" + sku + @"\" + i + ".jpg");
+                }
+                catch (WebException e)
+                {
+                    System.Diagnostics.Debug.Write(e.Message);
+                }
+            }
+        }
+    }
+
+    // Get/Set the website to crawl
+    class AgilityPackWebsite
+    {
+        string _website;
+        public string Website
+        {
+            get
+            {
+                return this._website;
+            }
+            set
+            {
+                this._website = value;
+            }
+        }
+    }
+
+
+    // Get/Set the fusking criteria
+    class FuskMethodCriteriaProperties
+    {
+        int _startProperty;
+        public int Start
+        {
+            get
+            {
+                return this._startProperty;
+            }
+            set
+            {
+                this._startProperty = value;
+            }
+        }
+        int _endProperty;
+        public int End
+        {
+            get
+            {
+                return this._endProperty;
+            }
+            set
+            {
+                this._endProperty = value;
+            }
+        }
+
+    }
+}
