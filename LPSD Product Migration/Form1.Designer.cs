1ef0d8a007c29e238503de133bd6fd3d329e1560
Anthony Hill <pimsh0@Hotmail.com>
2013-03-27 22:33:18 -0700

    Finish product migration program

--- LPSD Product Migration/Form1.Designer.cs
+++ LPSD Product Migration/Form1.Designer.cs
@@ -30,8 +30,10 @@
         {
             this.startButton = new System.Windows.Forms.Button();
             this.outputLabel = new System.Windows.Forms.Label();
-            this.saveCSVButton = new System.Windows.Forms.Button();
+            this.pullCategoryButton = new System.Windows.Forms.Button();
             this.outputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // startButton
@@ -40,7 +42,7 @@
             this.startButton.Name = "startButton";
             this.startButton.Size = new System.Drawing.Size(184, 23);
             this.startButton.TabIndex = 0;
-            this.startButton.Text = "Start Pulling Data";
+            this.startButton.Text = "Start Pulling Product Data";
             this.startButton.UseVisualStyleBackColor = true;
             this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
@@ -53,14 +55,15 @@
             this.outputLabel.TabIndex = 2;
             this.outputLabel.Text = "Output";
             // 
-            // saveCSVButton
+            // pullCategoryButton
             // 
-            this.saveCSVButton.Location = new System.Drawing.Point(254, 295);
-            this.saveCSVButton.Name = "saveCSVButton";
-            this.saveCSVButton.Size = new System.Drawing.Size(184, 23);
-            this.saveCSVButton.TabIndex = 3;
-            this.saveCSVButton.Text = "Save CSV File";
-            this.saveCSVButton.UseVisualStyleBackColor = true;
+            this.pullCategoryButton.Location = new System.Drawing.Point(254, 295);
+            this.pullCategoryButton.Name = "pullCategoryButton";
+            this.pullCategoryButton.Size = new System.Drawing.Size(184, 23);
+            this.pullCategoryButton.TabIndex = 3;
+            this.pullCategoryButton.Text = "Start Pulling Category Data";
+            this.pullCategoryButton.UseVisualStyleBackColor = true;
+            this.pullCategoryButton.Click += new System.EventHandler(this.pullCategoryButton_Click);
             // 
             // outputRichTextBox
             // 
@@ -70,17 +73,27 @@
             this.outputRichTextBox.TabIndex = 4;
             this.outputRichTextBox.Text = "";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(132, 327);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(692, 383);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.outputRichTextBox);
-            this.Controls.Add(this.saveCSVButton);
+            this.Controls.Add(this.pullCategoryButton);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.startButton);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,8 +103,9 @@
 
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Label outputLabel;
-        private System.Windows.Forms.Button saveCSVButton;
+        private System.Windows.Forms.Button pullCategoryButton;
         private System.Windows.Forms.RichTextBox outputRichTextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- LPSD Product Migration/Form1.cs
+++ LPSD Product Migration/Form1.cs
@@ -11,6 +11,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
+using System.Text.RegularExpressions;
 
 
 /* Saving for delegation method
@@ -46,6 +47,95 @@ namespace LPSD_Migration
             this.workerThread.Start();
         }
 
+        private void pullCategoryButton_Click(object sender, EventArgs e)
+        {
+            // Load up all the cat data send it to Download
+            // A little sloppy due to deadline
+            string website = "http://www.lockhartphillipsusa.com/store/image.php?type=C&id=";
+            string categoryIdData = "";
+            using (StreamReader reader = new StreamReader(@"C:\LPUSA Migration\categories\catIDs.txt"))
+            {
+                categoryIdData = reader.ReadToEnd();
+            }
+
+            string[] categoryIds = Regex.Split(categoryIdData,"\r\n");
+
+            byte[] data = new byte[50000];
+            Image _Image = null;
+            // Download
+            foreach (string category in categoryIds)
+            {
+                string conCatAddress = website + category;
+                
+                try
+                {
+                    _Image = DownloadImage(conCatAddress);
+                }
+                catch (WebException r)
+                {
+                    System.Diagnostics.Debug.Write(r.Message);
+                    continue;
+                }
+                // check for valid image
+                if (_Image != null)
+                {
+                    // lets save image to disk
+                    System.IO.Directory.CreateDirectory(@"C:\LPUSA Migration\categories\" + category);
+                    _Image.Save(@"C:\LPUSA Migration\categories\" + category + @"\main.jpg");
+                }
+                using (StreamWriter writer = new StreamWriter(@"C:\LPUSA Migration\categories\catImport.txt", true))
+                {
+                    writer.WriteLine(category + ";/home/lockhart/public_html/images/migration/categories/" + category + "/main.jpg");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Function to download Image from website
+        /// </summary>
+        /// <param name="_URL">URL address to download image</param>
+        /// <returns>Image</returns>
+        public Image DownloadImage(string _URL)
+        {
+            Image _tmpImage = null;
+
+            try
+            {
+                // Open a connection
+                System.Net.HttpWebRequest _HttpWebRequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(_URL);
+
+                _HttpWebRequest.AllowWriteStreamBuffering = true;
+
+                // You can also specify additional header values like the user agent or the referer: (Optional)
+                _HttpWebRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)";
+                _HttpWebRequest.Referer = "http://www.google.com/";
+
+                // set timeout for 20 seconds (Optional)
+                _HttpWebRequest.Timeout = 60000;
+
+                // Request response:
+                System.Net.WebResponse _WebResponse = _HttpWebRequest.GetResponse();
+
+                // Open data stream:
+                System.IO.Stream _WebStream = _WebResponse.GetResponseStream();
+
+                // convert webstream to image
+                _tmpImage = Image.FromStream(_WebStream);
+
+                // Cleanup
+                _WebResponse.Close();
+                _WebResponse.Close();
+            }
+            catch (Exception _Exception)
+            {
+                // Error
+                Console.WriteLine("Exception caught in process: {0}", _Exception.ToString());
+                return null;
+            }
+
+            return _tmpImage;
+        }
+
         // Set the fusk criteria
         public void FuskSetup()
         {
@@ -54,8 +144,8 @@ namespace LPSD_Migration
             agility.Website = "http://www.lockhartphillipsusa.com/store/product.php?productid=";
 
             // Ints only
-            fusk.Start = 17028;
-            fusk.End = 17029;
+            fusk.Start = 17135;
+            fusk.End = 21000;
 
             // Prep the fusk list with url prepended from agility
             List<string> fuskList = new List<string>();
@@ -72,11 +162,10 @@ namespace LPSD_Migration
         {
             WebClient client = new WebClient();
 
-            string htmlCode;
-
             // Pull HTML Code
             foreach (string fuskUrl in fuskList)
             {
+                string htmlCode = "";
                 try
                 {
                     htmlCode = client.DownloadString(fuskUrl);
@@ -84,6 +173,10 @@ namespace LPSD_Migration
                 catch (WebException)
                 {
                     continue;
+                }
+                finally
+                {
+                    client.Dispose();
                 }
                 // URL is good, let's parse it with agilitypack and get the data we want.
                 StartParsingWithAgilityPack(htmlCode);
@@ -114,11 +207,23 @@ namespace LPSD_Migration
             }
             // Parse for the detailed images
             List<string> detailedImages = new List<string>();
-            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//*[@class=\"detailed-images-other\"]"))
+            try
             {
-                HtmlAttribute att = link.Attributes["src"];
-                detailedImages.Add(att.Value); // This is parsing & signs with &amp; must be aware of this.
-                //System.Diagnostics.Debug.Write(att.Value + "\n");
+                foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//*[@class=\"detailed-images-other\"]"))
+                {
+                    HtmlAttribute att = link.Attributes["src"];
+                    detailedImages.Add(att.Value); // This is parsing & signs with &amp; must be aware of this.
+                    //System.Diagnostics.Debug.Write(att.Value + "\n");
+                }
+            }
+            catch
+            {
+                // This product doesn't have a detailed image, use main image
+                foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//*[@id=\"product_thumbnail\"]"))
+                {
+                    HtmlAttribute att = link.Attributes["src"];
+                    detailedImages.Add(att.Value);
+                }
             }
             // Everything is parsed send it to post processing
             ProcessTheProductDataAndDownloadImages(productSku, mainImageUrl, detailedImages);
@@ -149,14 +254,20 @@ namespace LPSD_Migration
                 System.Diagnostics.Debug.Write(e.Message);
             }
 
+            client.Dispose();
+            using (StreamWriter writer = new StreamWriter(@"C:\LPUSA Migration\main_image.txt", true))
+            {
+                writer.WriteLine(sku + ";" + @"images/P/{0}/main.jpg", sku);
+            }
+
             // Download detailed images and write them
-            string nonLocalDetailedImageUrl = "";
+            //string nonLocalDetailedImageUrl = "";
             for (int i = 0; i < detailedImages.Count; i++)
             {
                 // &amp; fix
                 detailedImages[i] = detailedImages[i].Replace("&amp;", "&");
 
-                nonLocalDetailedImageUrl = detailedImages[i];
+                //nonLocalDetailedImageUrl = detailedImages[i];
                 // Remove broken image links
                 if (detailedImages[i] == "/store/default_image.gif")
                 {
@@ -170,6 +281,15 @@ namespace LPSD_Migration
                 catch (WebException e)
                 {
                     System.Diagnostics.Debug.Write(e.Message);
+                }
+                finally
+                {
+                    client.Dispose();
+                    using (StreamWriter writer = new StreamWriter(@"C:\LPUSA Migration\detailed.txt", true))
+                    {
+                        //PATH is temp, will do find and replace on this
+                        writer.WriteLine(sku + ";" + @"images/D/{0}/{1}.jpg", sku, i);
+                    }
                 }
             }
         }
